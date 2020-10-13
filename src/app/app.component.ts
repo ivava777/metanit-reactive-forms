47b48c6024e455a63bfe3e3f5432d9d6d8d55fb8
ivava777 <ivava777@gmail.com>
2020-10-13 15:34:01 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'metanit-reactive-forms';
+    public myForm: FormGroup;
+    constructor(){
+        this.myForm = new FormGroup({
+            'userName': new FormControl('Tom', [Validators.required, this.userNameValidator]),
+            'userEmail': new FormControl('', [
+                                Validators.required,
+                                Validators.email
+                            ]),
+            "userInn": new FormControl('12345678901234', Validators.pattern('[0-9]{14}')),
+            "phones": new FormArray([
+              new FormControl('+38', Validators.required)
+            ])
+        });
+    }
+
+    submit(): void{
+        console.log(this.myForm);
+    }
+
+    // валидатор
+    userNameValidator(control: FormControl): {[s: string]: boolean}{
+      if (control.value === 'нет'){
+          return {'userName': true};
+      }
+      return null;
+  }
+
+  addPhone(): void{
+    (this.myForm.controls['phones'] as FormArray).push(new FormControl('+38', Validators.required));
+  }
 }
